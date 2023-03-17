b20cee1cf54b808eddfaabe58c3f5ef002c15a42
joaco <joacomda@gmail.com>
2023-03-17 23:58:47 +0100

    test workflow cd

--- backend/src/main/java/com/joaquingrandiccelli/PingPongController.java
+++ backend/src/main/java/com/joaquingrandiccelli/PingPongController.java
@@ -11,7 +11,6 @@ public class PingPongController {
 
     @GetMapping("/ping")
     public PingPong getPingPong() {
-        return new PingPong("Pong!!");
-
+        return new PingPong("Pong!!!!");
     }
 }
