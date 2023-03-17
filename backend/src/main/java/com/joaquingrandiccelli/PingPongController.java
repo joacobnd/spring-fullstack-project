319470bd6f4acccb64e9810bbe78009c867485fe
joaco <joacomda@gmail.com>
2023-03-17 23:44:12 +0100

    test new cd deploy

--- backend/src/main/java/com/joaquingrandiccelli/PingPongController.java
+++ backend/src/main/java/com/joaquingrandiccelli/PingPongController.java
@@ -11,6 +11,7 @@ public class PingPongController {
 
     @GetMapping("/ping")
     public PingPong getPingPong() {
-        return new PingPong("Pong");
+        return new PingPong("Pong!!");
+
     }
 }
