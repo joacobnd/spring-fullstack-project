4193b220260b5a779971cd01230d9700e0ee84a2
joaco <joacomda@gmail.com>
2023-03-23 11:03:42 +0100

    CORS, Axios, Fetch Customers, SideBard y Card Component

---
+++ backend/src/main/java/com/joaquingrandiccelli/config/WebMvcConfig.java
@@ -0,0 +1,25 @@
+package com.joaquingrandiccelli.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
+
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+
+    @Value("#{'${cors.allowed-origins}'.split(',')}")
+    private List<String> allowOrigins;
+    @Value("#{'${cors.allowed-methods}'.split(',')}")
+    private List<String> allowMethods;
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        CorsRegistration corsRegistration = registry.addMapping("/api/**");
+        allowOrigins.forEach(corsRegistration::allowedOrigins);
+        allowMethods.forEach(corsRegistration::allowedMethods);
+    }
+}
