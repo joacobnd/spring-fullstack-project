1a5c7518ed37cf3b9db3351e2a1e63e18bb86bed
joaco <joacomda@gmail.com>
2023-03-28 13:20:24 +0200

    try2 to fix cors blocks

--- backend/src/main/java/com/joaquingrandiccelli/config/WebMvcConfig.java
+++ backend/src/main/java/com/joaquingrandiccelli/config/WebMvcConfig.java
@@ -20,7 +20,7 @@ public class WebMvcConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        CorsRegistration corsRegistration = registry.addMapping("/api/**");
+        CorsRegistration corsRegistration = registry.addMapping("/api/**").allowedMethods("*").allowedMethods("*");
         allowOrigins.forEach(corsRegistration::allowedOrigins);
         allowMethods.forEach(corsRegistration::allowedMethods);
     }
