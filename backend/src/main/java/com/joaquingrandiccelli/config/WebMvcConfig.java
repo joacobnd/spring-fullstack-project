941495b13b59601d4eac3b147a12f95a86b2af79
joaco <joacomda@gmail.com>
2023-03-29 10:01:05 +0200

    fix5 cors

--- backend/src/main/java/com/joaquingrandiccelli/config/WebMvcConfig.java
+++ backend/src/main/java/com/joaquingrandiccelli/config/WebMvcConfig.java
@@ -7,12 +7,13 @@ import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.List;
 
 @Configuration
 @EnableWebMvc
-public class WebMvcConfig {
+public class WebMvcConfig implements WebMvcConfigurer {
 
     @Value("#{'${cors.allowed-origins}'.split(',')}")
     private List<String> allowedOrigins;
