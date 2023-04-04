630d7da2251cb4e2148061b19df35c0f6ad0a665
JoaquinGrandiccelli <joacomda@gmail.com>
2023-04-04 11:33:35 +0200

    Exercise: allow /ping endpoint

--- backend/src/main/java/com/joaquingrandiccelli/customer/CustomerController.java
+++ backend/src/main/java/com/joaquingrandiccelli/customer/CustomerController.java
@@ -3,9 +3,6 @@ package com.joaquingrandiccelli.customer;
 import com.joaquingrandiccelli.jwt.JWTUtil;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
--- backend/src/main/java/com/joaquingrandiccelli/security/SecurityFilterChainConfig.java
+++ backend/src/main/java/com/joaquingrandiccelli/security/SecurityFilterChainConfig.java
@@ -44,6 +44,8 @@ public class SecurityFilterChainConfig {
                         "/api/v1/auth/login"
                 )
                 .permitAll()
+                .requestMatchers(HttpMethod.GET,"/ping")
+                .permitAll()
                 .anyRequest()
                 .authenticated()
                 .and()
